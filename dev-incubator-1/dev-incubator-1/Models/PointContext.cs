0d594fbab69b8a52e7e27ea08b8324d5fd019c5e
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-08-28 05:31:14 +0300

    Feat add PointContext

---
+++ dev-incubator-1/dev-incubator-1/Models/PointContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace dev_incubator_1.Models
+{
+    public class PointContext : DbContext
+    {
+        public DbSet<Point> Points { get; set; }
+
+        public DbSet<UserData> UserDatas { get; set; }
+    }
+}
