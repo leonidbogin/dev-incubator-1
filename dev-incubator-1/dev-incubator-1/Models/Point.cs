d404ad1fcd6cd7fbe3796bb1534d64a0730a56d9
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-08-28 05:22:56 +0300

    Feat add models
    add Point model
    add UserData model

---
+++ dev-incubator-1/dev-incubator-1/Models/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dev_incubator_1.Models
+{
+    public class Point
+    {
+        public int PointId { get; set; }
+        public UserData ChartId { get; set; }
+        public int PointX { get; set; }
+        public int PointY { get; set; }
+    }
+}
---
+++ dev-incubator-1/dev-incubator-1/Models/UserData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dev_incubator_1.Models
+{
+    public class UserData
+    {
+        public int UserDataId { get; set; }
+        public int RangeFrom { get; set; }
+        public int RangeTo { get; set; }
+        public float Step { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+    }
+}
