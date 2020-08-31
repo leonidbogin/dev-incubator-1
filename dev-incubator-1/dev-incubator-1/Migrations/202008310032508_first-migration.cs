b3367a370ffcd18d831cefc2d08e08ec9534701a
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-08-31 03:34:41 +0300

    Feat add start-migration

---
+++ dev-incubator-1/dev-incubator-1/Migrations/202008310032508_first-migration.cs
@@ -0,0 +1,47 @@
+﻿namespace dev_incubator_1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstmigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Points",
+                c => new
+                    {
+                        PointId = c.Int(nullable: false, identity: true),
+                        PointX = c.Double(nullable: false),
+                        PointY = c.Double(nullable: false),
+                        ChartId_UserDataId = c.Int(),
+                    })
+                .PrimaryKey(t => t.PointId)
+                .ForeignKey("dbo.UserDatas", t => t.ChartId_UserDataId)
+                .Index(t => t.ChartId_UserDataId);
+            
+            CreateTable(
+                "dbo.UserDatas",
+                c => new
+                    {
+                        UserDataId = c.Int(nullable: false, identity: true),
+                        RangeFrom = c.Int(),
+                        RangeTo = c.Int(),
+                        Step = c.Single(),
+                        A = c.Int(),
+                        B = c.Int(),
+                        C = c.Int(),
+                    })
+                .PrimaryKey(t => t.UserDataId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Points", "ChartId_UserDataId", "dbo.UserDatas");
+            DropIndex("dbo.Points", new[] { "ChartId_UserDataId" });
+            DropTable("dbo.UserDatas");
+            DropTable("dbo.Points");
+        }
+    }
+}
